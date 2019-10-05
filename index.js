61337d5f3d2cdd028c51cc5147fa902fdf8ddb1a
sang park <kenversusryu@spark1.local>
2019-10-05 13:26:16 -0700

    committing first code

---
+++ index.js
@@ -0,0 +1,28 @@
+const config = require("./src/config");
+const Twit = require("twit");
+
+const Bot = new Twit(config);
+var tenSeconds = 10000;
+
+function tweet(txt) {
+  Bot.post(
+    "statuses/update",
+    {
+      status: txt
+    },
+    (err, data, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`${data.text} tweeted!`);
+      }
+    }
+  );
+}
+
+function tweetRandomNumber() {
+  var randomNumber = Math.floor(Math.random() * 1000);
+  tweet(randomNumber);
+}
+
+setInterval(tweetRandomNumber, tenSeconds);
